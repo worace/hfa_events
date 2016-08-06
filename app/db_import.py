be8cb7002473254ada1d143b908a13d9d502da07
Horace Williams <horace.d.williams@gmail.com>
2016-08-06 10:13:24 -0600

    move importing task to its own file

---
+++ app/db_import.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def run_command(cmd):
+    p = subprocess.Popen(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    output = []
+    for line in p.stdout.readlines():
+        output.append(line)
+
+    p.wait()
+    return "\n".join(output)
+
+def drop_db(db_name):
+    return run_command("psql -c 'DROP DATABASE %s';" % db_name)
+
+def list_tables():
+    return run_command("psql -P pager=off -c '\l'")
+
+def import_db(db_name, dump_file):
+    run_command("createdb %s" % db_name)
+    run_command("psql %s < %s" % (db_name, dump_file))
--- tests/db_import_tests.py
+++ tests/db_import_tests.py
@@ -1,42 +1,13 @@
 from nose.tools import *
-import subprocess
-import os
+import app.db_import as db
 
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR DOWN!"
-
-def run_command(cmd):
-    p = subprocess.Popen(cmd,
-                         shell=True,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-    output = []
-    for line in p.stdout.readlines():
-        output.append(line)
-
-    p.wait()
-    return "\n".join(output)
-
-def drop_db(db_name):
-    return run_command("psql -c 'DROP DATABASE %s';" % db_name)
-
-def list_tables():
-    return run_command("psql -P pager=off -c '\l'")
-
-def import_db(db_name, dump_file):
-    run_command("createdb %s" % db_name)
-    run_command("psql %s < %s" % (db_name, dump_file))
-
-def test_basic():
-    drop_db("hfa_events")
-    tables = list_tables()
-    assert("hfa_events" not in tables)
-    import_db("hfa_events", "./assignment/data.pgdump")
-    tables = list_tables()
-    assert("hfa_events" in tables)
-    count = run_command('psql -d hfa_events -c "select count(*) from events;"')
+def test_verifying_db_import():
+    assert True
+    db.drop_db("hfa_events")
+    assert("hfa_events" not in db.list_tables())
+    db.import_db("hfa_events", "./assignment/data.pgdump")
+    assert("hfa_events" in db.list_tables())
+    cmd = 'psql -d hfa_events -c "select count(*) from events;"'
+    count = db.run_command(cmd)
     assert("22" in count)
 
