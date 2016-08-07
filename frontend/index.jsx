9987f3e7abbaf304d9955248846097f86f630cb9
Horace Williams <horace.d.williams@gmail.com>
2016-08-07 10:19:23 -0600

    pull in event descriptions and basic expand/collapse logic

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -41,6 +41,9 @@ const EventsList = React.createClass({
 });
 
 const EventItem = React.createClass({
+  getInitialState: function() {
+    return {isExpanded: false};
+  },
   formattedDate: function() {
     let start = this.props.event.start_date;
     return moment(start).format("ddd, MMMM Do YYYY");
@@ -71,25 +74,53 @@ const EventItem = React.createClass({
         return count + " people going";
     }
   },
+  details: function() {
+    if (this.state.isExpanded) {
+      return <p>{this.props.event.description}</p>
+    } else {
+      return null;
+    }
+  },
+  moreOrLess: function() {
+    let text = {true: "Less", false: "More"}[this.state.isExpanded];
+    let toggle = function() {
+      this.setState({isExpanded: !this.state.isExpanded});
+    }.bind(this);
+
+    return <button className="btn btn-default" type="submit" onClick={toggle}>{text}</button>
+  },
   render: function() {
     return(
       <div className="row event">
-        <h3>{this.props.event.name}</h3>
-        <div className="col-sm-2">
-          <p>{this.formattedDate()}</p>
-          <p>{this.formattedStartTime()} to {this.formattedEndTime()}</p>
+        <div className="row col-sm-12">
+          <h3 className="col-sm-12">{this.props.event.name}</h3>
         </div>
 
-        <div className="col-sm-6">
-          <p>{this.props.event.location_info.address1}</p>
-          <p>{this.cityState()}</p>
+        <div className="row col-sm-12">
+          <div className="col-sm-2">
+            <p>{this.formattedDate()}</p>
+            <p>{this.formattedStartTime()} to {this.formattedEndTime()}</p>
+          </div>
+
+          <div className="col-sm-6">
+            <p>{this.props.event.location_info.address1}</p>
+            <p>{this.cityState()}</p>
+          </div>
+
+          <div className="col-sm-3">
+            <p>{this.signedUpCount()}</p>
+            <p>Are You Going?</p>
+            <button className="btn btn-default" type="submit">No</button>
+            <button className="btn btn-default" type="submit">Yes</button>
+          </div>
         </div>
 
-        <div className="col-sm-3">
-          <p>{this.signedUpCount()}</p>
-          <p>Are You Going?</p>
-          <button className="btn btn-default" type="submit">No</button>
-          <button className="btn btn-default" type="submit">Yes</button>
+        <div className="row col-sm-12">
+          <div className="col-sm-1">
+            {this.moreOrLess()}
+          </div>
+
+          {this.details()}
         </div>
 
       </div>
