14d96021046031417bae9b5caf22f2da93b602b9
Horace Williams <horace.d.williams@gmail.com>
2016-08-07 17:36:29 -0600

    parameterize api url as config value

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,18 +1,24 @@
 import $ from "jquery";
 
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000"
+
+function apiURL(path) {
+  return API_BASE_URL + path;
+}
+
 let API = {
   getEvents: function(pageNumber) {
-    return $.get("http://localhost:5000/events?page=" + (pageNumber || 1));
+    return $.get(apiURL("/events?page=" + (pageNumber || 1)));
   },
   eventRSVP: function(eventID, userInfo) {
-    let url = "http://localhost:5000/events/" + eventID + "/attendees"
+    let url = apiURL("/events" + eventID + "/attendees");
     return $.ajax({url: url,
                    method: "POST",
                    data: JSON.stringify(userInfo),
                    contentType: "application/json" });
   },
   eventUnRSVP: function(eventID, userInfo) {
-    let url = "http://localhost:5000/events/" + eventID + "/attendees"
+    let url = apiURL("/events" + eventID + "/attendees");
     return $.ajax({url: url,
                    method: "DELETE",
                    data: JSON.stringify(userInfo),
