b11e5327bf486e3e359ee48212de2e4e091a8dc7
Horace Williams <horace.d.williams@gmail.com>
2016-08-07 08:12:20 -0600

    salvage some notes from other frontend repo

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import API from "./lib/api";
 
+/* - Render the list of events in chronological order,
+     displaying whatever details you deem necessary for each event
+ * - Allow a user to view more details about each event,
+   including ticket tiers, event description, etc.
+ * - You can display the details on the same page, or create a second "event details" page.
+ * - Allow a user to mark an event as "attending"
+   - if a user has marked an event as "attending," this state should be reflected in the list of events, as well as on the event details page.
+ * */
+
 const EventsList = React.createClass({
   getInitialState: function() {
     console.log("will get events..")
